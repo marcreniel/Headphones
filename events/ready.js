9b25c96bdb620a8aeec358e12545c2c1b97d3b10
Marc Bernardino <marcbernardino2005@gmail.com>
2022-08-24 23:11:09 -0500

    merged v4 into main

--- events/ready.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-		const serv = client.guilds.cache.size;
-		client.user.setPresence({ activities: [{ name: `Playing music on ${serv} servers | /play` }], status: 'online' });
-		console.log('Headphones is ready! Project by Marc Bernardino - http://notmarc.me');
-	},
-};
