5ff24f8d65685e5fccb39ae3ae5112a0f0d382a4
Marc Bernardino <marcbernardino2005@gmail.com>
2022-08-24 23:17:57 -0500

    final merge to v4

--- commands/queue.js
+++ commands/queue.js
@@ -11,43 +11,21 @@ module.exports = {
 
         if (!queue) {
 			const noQueue = new MessageEmbed()
-<<<<<<< HEAD
 			.setAuthor({name:'Headphones', iconURL: 'https://media.discordapp.net/attachments/929899694560280627/990819878535589939/Headphonesv4Logo.png'})
-=======
-			.setAuthor('Headphones', 'https://media.discordapp.net/attachments/887886467215544333/887886502833569812/HPL.png?width=671&height=671')
->>>>>>> 477328c (added pause, unpause, queue functionality)
 			.setDescription(':x: There is nothing currently in the queue.')
 			.setColor('PURPLE');
 			return interaction.reply({ embeds: [noQueue], ephemeral: true });
 		}
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 		const csong = queue.songs[0];
-=======
->>>>>>> 477328c (added pause, unpause, queue functionality)
-=======
-		const csong = queue.songs[0];
->>>>>>> 4023400 (final dev build: added volume, seek, previous, full playlist support)
         const list = queue.songs
         .map((song, i) => `${i === 0 ? '**Now Playing:**' : `**${i}**.`} ${song.name} - \`${song.formattedDuration}\` - Requested by ${song.user}`)
         .join('\n');
         const queueList = new MessageEmbed()
-<<<<<<< HEAD
 		.setAuthor({name:'Headphones', iconURL: 'https://media.discordapp.net/attachments/929899694560280627/990819878535589939/Headphonesv4Logo.png'})
         .setColor('PURPLE')
 		.setThumbnail(csong.thumbnail)
         .setDescription(list.slice(0, 1000) + '\n **... And more songs that cannot fit**', false);
-=======
-		.setAuthor('Headphones | Song Queue', 'https://media.discordapp.net/attachments/887886467215544333/887886502833569812/HPL.png?width=671&height=671')
-        .setColor('PURPLE')
-<<<<<<< HEAD
-        .setDescription(list, false);
->>>>>>> 477328c (added pause, unpause, queue functionality)
-=======
-		.setThumbnail(csong.thumbnail)
-        .setDescription(list.slice(0, 1000) + '\n **... And more songs that cannot fit**', false);
->>>>>>> 4023400 (final dev build: added volume, seek, previous, full playlist support)
         interaction.reply({ embeds: [queueList] });
     },
 };
