4530faf835d8ebe86e2ad38951fefff58ec0c226
Marc Reniel Bernardino <marc@Marcs-MacBook-Pro.local>
2022-08-30 13:55:24 -0500

    fixed merge, updated pkg, added spotify support

--- commands/pause.js
+++ commands/pause.js
@@ -12,14 +12,14 @@ module.exports = {
 
 		if (!channel) {
 			const embedJoin = new MessageEmbed()
-			.setAuthor('Headphones', 'https://media.discordapp.net/attachments/887886467215544333/887886502833569812/HPL.png?width=671&height=671')
+			.setAuthor({name:'Headphones', iconURL:'https://media.discordapp.net/attachments/929899694560280627/990819878535589939/Headphonesv4Logo.png',})
 			.setDescription(':x: You must join a voice channel to use this command!')
 			.setColor('PURPLE');
             return interaction.reply({ embeds: [embedJoin], ephemeral: true });
         }
 		if (!queue) {
 			const noQueue = new MessageEmbed()
-			.setAuthor('Headphones', 'https://media.discordapp.net/attachments/887886467215544333/887886502833569812/HPL.png?width=671&height=671')
+			.setAuthor({name:'Headphones', iconURL:'https://media.discordapp.net/attachments/929899694560280627/990819878535589939/Headphonesv4Logo.png',})
 			.setDescription(':x: There is nothing currently playing!')
 			.setColor('PURPLE');
 			return interaction.reply({ embeds: [noQueue], ephemeral: true });
@@ -27,7 +27,7 @@ module.exports = {
 		if (queue) {
             if (interaction.member.guild.me.voice.channelId !== interaction.member.voice.channelId) {
 			const embedSameChannel = new MessageEmbed()
-			.setAuthor('Headphones', 'https://media.discordapp.net/attachments/887886467215544333/887886502833569812/HPL.png?width=671&height=671')
+			.setAuthor({name:'Headphones', iconURL:'https://media.discordapp.net/attachments/929899694560280627/990819878535589939/Headphonesv4Logo.png',})
 			.setDescription(':x: I am already in a voice channel! Please join the one I am currently in.')
 			.setColor('PURPLE');
 			return interaction.reply({ embeds: [embedSameChannel], ephemeral: true });
@@ -35,7 +35,7 @@ module.exports = {
         }
 
 		const pause = new MessageEmbed()
-		.setAuthor('Headphones', 'https://media.discordapp.net/attachments/887886467215544333/887886502833569812/HPL.png?width=671&height=671')
+		.setAuthor({name:'Headphones', iconURL:'https://media.discordapp.net/attachments/929899694560280627/990819878535589939/Headphonesv4Logo.png',})
         .setDescription(`:pause_button: <@${interaction.user.id}> Has paused the music.`)
         .setColor('PURPLE');
 		await queue.pause();
